d831724cdd31952a6dbd7a32c0f64062cfe17efb
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-17 15:33:10 -0600

    adding in shell of parts 1 - 4

---
+++ project3/part2.cpp
@@ -0,0 +1,21 @@
+/*************************************
+ * Project 3
+ * Purpose: This is to answer the second part of the assignment
+ * Author: Josh Wretlind
+ * Date: 10/17/2013
+ *************************************/
+#include<iostream>
+#include "mpi.h"
+#include<string>
+
+using namespace std;
+using namespace MPI;
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
