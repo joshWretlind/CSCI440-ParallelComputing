733cee60bfa7213fbd957386b35f25524946adb2
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-18 17:48:10 -0600

    adding in code for part 1

--- project3/part1.cpp
+++ project3/part1.cpp
@@ -7,13 +7,33 @@
 #include<iostream>
 #include "mpi.h"
 #include<string>
+#include<vector>
 
 using namespace std;
 using namespace MPI;
 
+double function(int k, double x){
+}
 
+double calculateMiddleRienmann(int K, vector<double> points, double(*fn)(int, double), int qn){
+	double value = 0;
+	for(int i = 1; i < points.size(); i++){
+		value += (fn(K, (points(i-1) + points(i))/2) * (points(i) - points(i-1)));
+	}
+	return value;
+}
 
+double calculateTrapazoidalRienmann(int K, vector<double> points, double(*fn)(int, double), int qn){
+	double value = 0;
+	for(int i = 0; i < points.size(); i++){
+		value += 0.5 * (fn(K, points(i-1)) + fn(K, points(i))) * (points(i) - points(i-1));
+	}
+	return value;
+}
 
+double calculateSimonRule(int K, vector<double> points, double(*fn)(int, double) , int qn){
+	return ((2.0/3.0) * calculateMiddleRienmann(K,points,fn,qn) + (1.0/3.0) * calculateTrapazoidalRienmann(K,points,fn,qn));
+}
 
 int main(int argc, char *argv[]){
 
