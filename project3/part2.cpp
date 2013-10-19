2e9190cf5340ade762dcb078b661fba73ac20703
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-18 18:59:22 -0600

    adding in part 2

--- project3/part2.cpp
+++ project3/part2.cpp
@@ -7,11 +7,30 @@
 #include<iostream>
 #include "mpi.h"
 #include<string>
+#include<vector>
+#include<cmath>
 
 using namespace std;
 using namespace MPI;
 
-
+/*************************************
+ * function(int k, vector<double> points)
+ * Purpose: This is to calculate a collection of values corrosponding to points
+ *          action function it calculates: f(x) = cos(100x - k*sin(x))
+ * Parameters: int k: The wave number for the function we want to calculate
+ *             vector<double> points: the collection of points we want to evaluate the function on
+ * Output: vector<double> of the values corrosponsing with points
+ * Complexity: Time:  O(N)
+ *             Space: O(N) 
+ ***********************************/
+vector<double> function(int k, vector<double> points){
+	vector<double> results;
+	results.resize(points.size();
+	for(int i = 0; i < points.size(); i++){
+		results[i] = cos(100 * points[i] - k * sin(points[i]));
+	}
+	return results;
+}
 
 int main(int argc, char *argv[]){
 
