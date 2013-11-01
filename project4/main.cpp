21e630119663f611c7e5f0dfee0b1a6985f6f268
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-01 17:49:36 -0600

    adding in generation of random weighted vector, as well as some cleaning up of machine files and test of an input system

--- project4/main.cpp
+++ project4/main.cpp
@@ -1,9 +1,48 @@
 #include "mpi.h"
 #include <iostream>
+#include <vector>
+#include <random>
 
 using namespace std;
 
+int master = 0;
+int totalSize;
+int myRank;
+
+//Defined in the project description
+int j;
+
+//Total number of processors
+int p;
+
+/*****************************************************
+ * generateRandomWeightedVector
+ * Purpose: This method is to generate the size-dimentional random weighted
+ *          vector(this is called r_k in the assignment)
+ * Arguments: int size: this is the size that the vector should take
+ * Returns: double*: This is the generated vector;
+ * 
+ * ***************************************************/
+ double* generateRandomWeightedVector(int size){
+    default_random_engine generator;
+    double* rOfK = new double[size];
+    
+    for(int i = 0; i < size; i++){
+        uniform_real_distribution<double> distribution(((double)myRank)/((double)i),i * myRank + 1.0);
+        rOfK[i] = distribution(generator);
+    }
+    
+    return rOfK;
+ }
+
+
 int main(int argc, char *argv[]){
-    cout << "hi" << endl;
-    return 0;
+    MPI::Init(argc, argv);
+    
+    totalSize = MPI::COMM_WORLD.Get_size();
+    myRank = MPI::COMM_WORLD.Get_rank();
+    p = totalSize;
+    cout << argv[0];
+    
+	MPI::Finalize();
 }
