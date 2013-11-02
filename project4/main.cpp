36fae56a9cd3f311b1ba1f260be4b0b0a7b48e12
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-02 17:17:06 -0600

    making the debug out easier to read

--- project4/main.cpp
+++ project4/main.cpp
@@ -184,6 +184,7 @@ int main(int argc, char *argv[]){
             }
             cout << endl;
         }
+        cout << "-----------------------------------------" << endl;
         for(int i = 0; i < j; i++){
             cout << "MyRank: " << myRank;
             for(int k = 0; k < p*j; k++){
