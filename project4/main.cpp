71ea589d77ca80a725f0e1d12635d98003a34917
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-02 18:44:01 -0600

    fixing this

--- project4/main.cpp
+++ project4/main.cpp
@@ -184,9 +184,9 @@ int main(int argc, char *argv[]){
     }
     else{
         for(int i = j; i < p*j; i++){
-            cout << " trying to recv " << p + i << " from " << ceil(((double)i)/j) << endl;
+            cout << " trying to recv " << i << " from " << ceil(((double)i)/j) << endl;
             MPI::Status myStatus;
-            MPI::COMM_WORLD.Recv(yMatrix[i],p*j,MPI_DOUBLE,ceil(((double)i)/j),p+i,myStatus);
+            MPI::COMM_WORLD.Recv(yMatrix[i],p*j,MPI_DOUBLE,ceil(((double)i)/j),i,myStatus);
         }
     }
     
