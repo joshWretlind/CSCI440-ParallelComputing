ea8dd8ffe49c86cde8a0b1cea2ecf8b8b6d836f7
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-05 16:46:16 -0700

    Adding in more run onfo

--- final/main.cpp
+++ final/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 #include "mpi.h"
 
 using namespace std;
@@ -8,14 +9,19 @@ int totalSize;
 int myRank;
 
 int main(int argc, char *argv[]){
-	double startTime;
-	double endTime;   
+    double startTime;
+    double endTime;   
     
     //initialize Stuff
     MPI::Init(argc, argv);
     startTime = MPI::Wtime();
     totalSize = MPI::COMM_WORLD.Get_size();
     myRank = MPI::COMM_WORLD.Get_rank();
+    
+    int digest = atoi(argv[0]);
+    string message = argv[1];
+    
+    cout << message << endl;
     
     
     //Finish things, clean up after ourselves.
