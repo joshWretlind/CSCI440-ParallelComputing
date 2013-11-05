8db3443101719fd28b7514fa7a498bf2cf330ba6
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-05 10:20:26 -0700

    addinng in initial final project shell

--- final/main.cpp
+++ final/main.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include "mpi.h"
 
 using namespace std;
-using namespace MPI;
 
-int main(){
-	
+int master = 0;
+int totalSize;
+int myRank;
+
+int main(int argc, char *argv[]){
+	double startTime;
+	double endTime;   
+    
+    //initialize Stuff
+    MPI::Init(argc, argv);
+    startTime = MPI::Wtime();
+    totalSize = MPI::COMM_WORLD.Get_size();
+    myRank = MPI::COMM_WORLD.Get_rank();
+    
+    
+    //Finish things, clean up after ourselves.
+    endTime = MPI::Wtime();
+	MPI::Finalize();
 }
