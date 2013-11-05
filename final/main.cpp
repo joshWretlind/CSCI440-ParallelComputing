7489045f9e834218a8b6051654119ae8daeeb251
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-05 16:48:48 -0700

    fixing includes from previous commit

--- final/main.cpp
+++ final/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include "mpi.h"
+#inlcude<cstdlib>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ int main(int argc, char *argv[]){
     totalSize = MPI::COMM_WORLD.Get_size();
     myRank = MPI::COMM_WORLD.Get_rank();
     
-    int digest = atoi(argv[0]);
+    int digest = atoi(argv[1]);
     string message = argv[1];
     
     cout << message << endl;
