f7754cb6502047dd78c1cde9bb40fc60f0e4e377
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-05 17:17:10 -0700

    adding in conversion to bits

--- final/main.cpp
+++ final/main.cpp
@@ -1,13 +1,59 @@
+/***************************************
+ * Author: Josh Wretlind
+ * Date: 11/05/13
+ * Purpose: This is the main file for my final project for CSCI 440
+ *          (Performing SHA-3 hashing in parallel)
+ ***************************************/
+
+
 #include<iostream>
 #include<string>
 #include "mpi.h"
 #include<cstdlib>
+#include<bitset>
 
 using namespace std;
 
 int master = 0;
 int totalSize;
 int myRank;
+
+/*******************************************************
+ * convertStringTobits
+ * Purpose: This method converts a string into straight binary bits
+ * Arguments: string str: This is the string we wish to convert
+ * Return value: bitset: the set of bits for the final result
+ * Complexity:  Time: O()
+ *             Space: O()
+ * *****************************************************/
+bitset convertStringToBits(string str){
+    char* cStr = str.c_str();
+    
+    chunkPerWorker = ceil(((double)str.length())/((double)totalSize));
+    lowerBound = rank*(chunkPerWorker);
+    upperBound = (rank+1)*(chunkPerWorker);
+    if((rank+1) == totalSize){
+	upperBound = str.length();
+    }
+    if(upperBound > str.length()){
+	upperBound = str.length();
+    }
+    if(lowerBound > str.length()){
+	lowerBound = str.length();
+    }
+    if(lowerBound == upperBound){
+	return;
+    }
+    bitset<8*(upperBound - lowerBound)> mainBitset;
+    for(size_t i =lowerBound; i < upperBound; i++){
+	bitset<8> currentChar = new bitset<8>(cStr[i]);
+	for(int j = 0; j < 8; j++){
+	    mainBitset.set(8*i + j, currentChar[j]);
+	} 
+    }
+    
+    return mainBitset;
+}
 
 int main(int argc, char *argv[]){
     double startTime;
@@ -18,6 +64,10 @@ int main(int argc, char *argv[]){
     startTime = MPI::Wtime();
     totalSize = MPI::COMM_WORLD.Get_size();
     myRank = MPI::COMM_WORLD.Get_rank();
+    
+    if(myRank == master){
+    
+    }
     
     int digest = atoi(argv[1]);
     string message = argv[2];
