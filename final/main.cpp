e346f1df1d948ee691896bcdd61e6ea9b08f0f3b
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-17 19:50:05 -0700

    trying this fix

--- final/main.cpp
+++ final/main.cpp
@@ -256,11 +256,11 @@ void setupRoundConstants(){
  * 
  * *******************************/
 void setupCyclicConstants(){
-    cycleOffset[0] = {0,1,62,28,27};
-    cycleOffset[1] = {36,44,6, 55,20};
-    cycleOffset[2] = {3,13,43,25,39};
-    cycleOffset[3] = {41,45,15,21,8};
-    cycleOffset[4] = {18,2,61,56,14};
+    *cycleOffset[0] = {0,1,62,28,27};
+    *cycleOffset[1] = {36,44,6, 55,20};
+    *cycleOffset[2] = {3,13,43,25,39};
+    *cycleOffset[3] = {41,45,15,21,8};
+    *cycleOffset[4] = {18,2,61,56,14};
 }
 
 int main(int argc, char *argv[]){
