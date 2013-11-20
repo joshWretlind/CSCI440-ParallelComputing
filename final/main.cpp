9c5b19188f17381daa43fcbf95e8f270768d3f95
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-19 17:08:35 -0700

    playing around with state information

--- final/main.cpp
+++ final/main.cpp
@@ -372,13 +372,13 @@ void permuteState(bool* message){
     for(int i = 0; i < paddedSize/r; i++){
 	bool tripped = false;
 	for(int j = 0; j < 5; j++){
-	    for(int k = 0; k < 5; k++){
+	    for(int k = 0; k < 5; k++){5**
 		if((5*k + j) >= (r/w)){
 		    tripped = true;
 		    break;
 		}
 		for(int l = 0; l < w; l++){
-			state[j][k][l] = state[j][k][l] ^ message[l + w*(5*k + j)];
+			state[j][k][l] = state[j][k][l] ^ message[i*r + l + w*k + 5*w*j];
 		}
 	    }
 	    if(tripped){
